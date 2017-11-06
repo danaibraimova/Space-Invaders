02192292c979ff675994ea65b83431f7e7e45648
danaibraimova <ibraimova.dana@gmail.com>
2017-11-06 19:06:28 +0600

    spaceship moving with touches and firing bullets

---
+++ Space Invaders/Space Invaders/Space Invaders/GameScene.swift
@@ -0,0 +1,61 @@
+//
+//  GameScene.swift
+//  Space Invaders
+//
+//  Created by user on 06.11.17.
+//  Copyright © 2017 KBTU. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit //
+
+class GameScene: SKScene {
+    let player = SKSpriteNode(imageNamed: "playerShip")
+    
+    let bulletSound = SKAction.playSoundFileNamed("bulletSoundEffect.wav", waitForCompletion: false)
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+        
+        player.setScale(0.3)
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
+        player.zPosition = 2 // bullet underneath
+        self.addChild(player)
+    }
+    
+    func fireBullet() {
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1 // behind the player
+        self.addChild(bullet)
+        
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        
+        let bulletSequence = SKAction.sequence([bulletSound, moveBullet, deleteBullet])
+        
+        bullet.run(bulletSequence)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireBullet()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let pointOfTouch = touch.location(in: self)
+            let previousPointOfTouch = touch.previousLocation(in: self)
+            
+            let amountDragged = pointOfTouch.x - previousPointOfTouch.x
+            
+            player.position.x += amountDragged
+            
+        }
+    }
+    
+}
