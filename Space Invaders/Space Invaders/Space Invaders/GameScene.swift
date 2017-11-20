8cd0de58cef6be1af58515482aea04f19f23d85e
danaibraimova <ibraimova.dana@gmail.com>
2017-11-20 21:53:41 +0600

    lives and level systems

--- Space Invaders/Space Invaders/Space Invaders/GameScene.swift
+++ Space Invaders/Space Invaders/Space Invaders/GameScene.swift
@@ -9,12 +9,54 @@
 import SpriteKit
 import GameplayKit //
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var gameScore = 0
+    let scoreLabel = SKLabelNode(fontNamed: "The Bold Font")
+    
+    var livesNumber = 3
+    let livesLabel = SKLabelNode(fontNamed: "The Bold Font")
+    
+    var levelNumber = 0
+    
     let player = SKSpriteNode(imageNamed: "playerShip")
     
     let bulletSound = SKAction.playSoundFileNamed("bulletSoundEffect.wav", waitForCompletion: false)
+    let explosionSound = SKAction.playSoundFileNamed("explosionSoundEffect.wav", waitForCompletion: false)
+    
+    struct PhysicsCategories {
+        static let None : UInt32 = 0
+        static let Player : UInt32 = 0b1
+        static let Bullet : UInt32 = 0b10
+        static let Enemy : UInt32 = 0b100 
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF )
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat { //
+        return random() * (max - min) + min
+    }
+    
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playableWidth = size.height / maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func didMove(to view: SKView) {
+        self.physicsWorld.contactDelegate = self
+        
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
         background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
@@ -24,14 +66,165 @@ class GameScene: SKScene {
         player.setScale(0.3)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
         player.zPosition = 2 // bullet underneath
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody!.affectedByGravity = false
+        player.physicsBody!.categoryBitMask = PhysicsCategories.Player
+        player.physicsBody!.collisionBitMask = PhysicsCategories.None
+        player.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(player)
+        
+        scoreLabel.text = "Score: 0"
+        scoreLabel.fontSize = 70
+        scoreLabel.fontColor = SKColor.white
+        scoreLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+        scoreLabel.position = CGPoint(x: self.size.width*0.15, y: self.size.height*0.9)
+        scoreLabel.zPosition = 100 // always be on top
+        self.addChild(scoreLabel)
+        
+        livesLabel.text = "Lives: 3"
+        livesLabel.fontSize = 70
+        livesLabel.fontColor = SKColor.white
+        livesLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.right
+        livesLabel.position = CGPoint(x: self.size.width*0.85, y: self.size.height*0.9)
+        livesLabel.zPosition = 100
+        self.addChild(livesLabel)
+        
+        startNewLevel()
+    }
+    
+    func loseLife() {
+        livesNumber -= 1
+        livesLabel.text = "Lives: \(livesNumber)"
+        
+        let scaleUp = SKAction.scale(to: 1.5, duration: 0.2)
+        let scaleDown = SKAction.scale(to: 1, duration: 0.2)
+        let scaleSequence = SKAction.sequence([scaleUp, scaleDown])
+        livesLabel.run(scaleSequence)
+        
+        if livesNumber == 0 {
+            runGameOver()
+        }
+    }
+    
+    func addScore() {
+        gameScore += 1
+        scoreLabel.text = "Score: \(gameScore)"
+        
+        if gameScore == 10 || gameScore == 25 || gameScore == 50 {
+            startNewLevel()
+        }
+    }
+    
+    func runGameOver() {
+        self.removeAllActions()
+        self.enumerateChildNodes(withName: "Bullet") {
+            bullet, stop in
+                bullet.removeAllActions()
+        }
+        
+        self.enumerateChildNodes(withName: "Enemy") {
+            enemy, stop in
+                enemy.removeAllActions()
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var body1 = SKPhysicsBody()
+        var body2 = SKPhysicsBody()
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            body1 = contact.bodyA
+            body2 = contact.bodyB
+        }
+        else {
+            body1 = contact.bodyB
+            body2 = contact.bodyA
+        }
+        
+        // if the player has hit the enemy
+        if body1.categoryBitMask == PhysicsCategories.Player && body2.categoryBitMask == PhysicsCategories.Enemy {
+            if body1.node != nil {
+                spawnExplosion(spawnPosition: body1.node!.position) // spawn explosion in the position of player
+            }
+            
+            if body2.node != nil {
+                spawnExplosion(spawnPosition: body2.node!.position) // in the position of enemy
+            }
+            
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+            
+            runGameOver()
+        }
+        
+        // if the bullet has hit the enemy
+        if body1.categoryBitMask == PhysicsCategories.Bullet && body2.categoryBitMask == PhysicsCategories.Enemy && (body2.node?.position.y)! < self.size.height {
+            
+            addScore()
+            
+            if body2.node != nil {
+                spawnExplosion(spawnPosition: body2.node!.position)
+            }
+            
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+        }
+        
+    }
+    
+    func spawnExplosion(spawnPosition: CGPoint) {
+        let explosion  = SKSpriteNode(imageNamed: "explosition")
+        explosion.position = spawnPosition
+        explosion.zPosition = 3
+        explosion.setScale(0)
+        self.addChild(explosion)
+        
+        let scaleIn = SKAction.scale(to: 1, duration: 0.1)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.1)
+        let delete = SKAction.removeFromParent()
+        
+        let explosionSequence = SKAction.sequence([explosionSound, scaleIn, fadeOut, delete])
+        
+        explosion.run(explosionSequence)
+    }
+    
+    func startNewLevel() {
+        levelNumber += 1
+        
+        if self.action(forKey: "spawningEnemies") != nil {
+            self.removeAction(forKey: "spawningEnemies")
+        }
+        
+        var levelDuration = TimeInterval()
+        
+        switch levelNumber {
+        case 1: levelDuration = 1.2
+        case 2: levelDuration = 1
+        case 3: levelDuration = 0.8
+        case 4: levelDuration = 0.5
+        default:
+            levelDuration = 0.5
+        }
+        
+        let spawn = SKAction.run(spawnEnemy)
+        let waitToSpawn = SKAction.wait(forDuration: levelDuration)
+        let spawnSequence = SKAction.sequence([waitToSpawn, spawn])
+        let spawnForever = SKAction.repeatForever(spawnSequence)
+        
+        self.run(spawnForever, withKey: "spawningEnemies")
     }
     
     func fireBullet() {
         let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.name = "Bullet"
         bullet.setScale(1)
         bullet.position = player.position
         bullet.zPosition = 1 // behind the player
+        bullet.physicsBody = SKPhysicsBody(rectangleOf: bullet.size)
+        bullet.physicsBody!.affectedByGravity = false
+        bullet.physicsBody!.categoryBitMask = PhysicsCategories.Bullet
+        bullet.physicsBody!.collisionBitMask = PhysicsCategories.None
+        bullet.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(bullet)
         
         let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
@@ -40,6 +233,37 @@ class GameScene: SKScene {
         let bulletSequence = SKAction.sequence([bulletSound, moveBullet, deleteBullet])
         
         bullet.run(bulletSequence)
+    }
+    
+    func spawnEnemy() {
+        let randomXStart = random(min: gameArea.minX, max: gameArea.maxX)
+        let randomXEnd = random(min: gameArea.minX, max: gameArea.maxX)
+        
+        let startPoint = CGPoint(x: randomXStart, y: self.size.height * 1.2)
+        let endPoint = CGPoint(x: randomXEnd, y: -self.size.height * 0.2)
+        
+        let enemy = SKSpriteNode(imageNamed: "enemyShip")
+        enemy.name = "Enemy"
+        enemy.setScale(0.4)
+        enemy.position = startPoint
+        enemy.zPosition = 2
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody!.affectedByGravity = false
+        enemy.physicsBody!.categoryBitMask = PhysicsCategories.Enemy
+        enemy.physicsBody!.collisionBitMask = PhysicsCategories.None
+        enemy.physicsBody!.contactTestBitMask = PhysicsCategories.Player | PhysicsCategories.Bullet
+        self.addChild(enemy)
+        
+        let moveEnemy = SKAction.move(to: endPoint, duration: 1.5)
+        let deleteEnemy = SKAction.removeFromParent()
+        let loseLifeAction = SKAction.run(loseLife)
+        let enemySequence = SKAction.sequence([moveEnemy, deleteEnemy, loseLifeAction])
+        enemy.run(enemySequence)
+        
+        let dx = endPoint.x - startPoint.x
+        let dy = endPoint.y - startPoint.y
+        let amountToRotate = atan2(dy, dx)
+        enemy.zRotation = amountToRotate
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -54,6 +278,15 @@ class GameScene: SKScene {
             let amountDragged = pointOfTouch.x - previousPointOfTouch.x
             
             player.position.x += amountDragged
+            
+            // чтобы корабль не выходил за границы экрана
+            if player.position.x > gameArea.maxX - player.size.width/2 {
+                player.position.x = gameArea.maxX - player.size.width/2
+            }
+            
+            if player.position.x < gameArea.minX + player.size.width/2  {
+                player.position.x = gameArea.minX + player.size.width/2
+            }
             
         }
     }
